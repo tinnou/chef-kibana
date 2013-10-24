a7917ba9e3031ad03ca721e096d52bacfa7b04b0
Wouter de Vos <wouter@springest.com>
2013-10-24 15:19:47 +0200

    Fix htpasswd filename.

--- recipes/htpasswd.rb
+++ recipes/htpasswd.rb
@@ -1,8 +1,8 @@
 include_recipe 'htpasswd'
 include_recipe 'nginx'
 
-# add user "foo" with password "bar" to "/etc/nginx/htpassword"
-htpasswd "/etc/nginx/htpassword" do
+# add user "foo" with password "bar" to "/etc/nginx/htpasswd"
+htpasswd "/etc/nginx/htpasswd" do
   user node['kibana']['web_user']
   password node['kibana']['web_password']
   notifies :reload, "service[nginx]"
